e1f869d40ef4aedc67622070e1f8828427bbd0e6
petre228 <medwedew.petr2017@yandex.ru>
2020-10-24 21:56:19 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import reg as r
+
+#new_user = registrate.Registration()
+
+new_user = r.Registration.create_new_acc(user_name='peter')
---
+++ reg.py
@@ -0,0 +1,6 @@
+class Registration(object):
+    def __init__(self):
+        pass
+
+    def create_new_acc(user_name):
+        print(user_name)
